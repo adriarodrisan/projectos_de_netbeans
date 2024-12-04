4645558c3a473241f7e9311b44b7b256a5904c77
adriarodrisan <1900adria@gmail.com>
2024-12-04 19:31:32 +0100

    canvios de la parte anterios del intento de id incremental

--- Casino/src/main/java/com/mvm/daw/casino/model/Apuesta.java
+++ Casino/src/main/java/com/mvm/daw/casino/model/Apuesta.java
@@ -1,8 +1,5 @@
 package com.mvm.daw.casino.model;
 
-
-import java.util.UUID;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
